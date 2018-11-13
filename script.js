7f99469ef5009a2231c5267df57fcf8ed72871f4
Nazmus Nasir <nazmus.nasir@gmail.com>
2018-11-12 21:29:51 -0500

    modified code to remove winners from array if multiple winners are requested

--- script.js
+++ script.js
@@ -11,21 +11,36 @@
 $(document).ready(function () {
     $('#submit').on('click', function(){
         
+        /*
+            show the output ID
+            reset #winnerOutput so it doesn't keep adding
+        */
         $('#output').show();
         $('#winnerOutput').html('');
         
-        //read input data
+        // read input data, trim the ends, and split into arrays by new line
         let initList = $('#winnersList').val().trim().split('\n');
-        let list = [];
+        let list = []; // initialize list array
         
-        // get rid of Blank Spaces
+        // get rid of Blank Spaces and push to list[] array
         $(initList).each(function(i, item){
            (item == '') ? '' : list.push(item);
         });
         
-        //pick random array index based on list.length i number of times
+        // pick random array index based on list.length i number of times
         for(let i = 0; i < $('#winnerQty').val(); i++){
-            let winner = list[Math.floor(Math.random() * (list.length))];
+            /* 
+                get random index number and grab value of winner
+                Source: https://www.easyprogramming.net/javascript/js_random_number_generator.php
+            */
+            let index = Math.floor(Math.random() * (list.length));
+            let winner = list[index];
+            
+            /*
+                remove the winner from the array on each loop iteration
+                This is so that someone who won isn't in the pool again for an unfair advantage
+            */
+            list.splice(index, 1);
             $('#winnerOutput').append($('<li>').append(winner));
         }
     });
